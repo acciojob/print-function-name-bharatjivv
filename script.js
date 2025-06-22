d93bd63c386407678a74840bf971f9986c9dba60
Bharat <hibharatj@gmail.com>
2025-06-22 17:52:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+// const functionName = function fn() {
+//     alert(fn.name);
+// };
+
+function functionName() {
+    alert(functionName.name);
+}
+
+
+functionName();
